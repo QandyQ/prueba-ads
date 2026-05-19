3387250db835b36694e139f5f02576dd055b463f
QandyQ <edwin.quishpe@espoch.edu.ec>
2026-05-19 15:35:01 -0500

    camgios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11029745
+    "domain": "5gvci.com",
+    "zoneId": 11030076
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
