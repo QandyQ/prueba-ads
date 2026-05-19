766efb9f80655a1f7d5c752d7f14ae541888d1a8
QandyQ <edwin.quishpe@espoch.edu.ec>
2026-05-19 13:45:17 -0500

    caions nuevos

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11029588
+    "zoneId": 11029726
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
