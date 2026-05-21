6e74e39840f5848fd474454e277f266eea8c68e5
QandyQ <edwin.quishpe@espoch.edu.ec>
2026-05-21 16:39:50 -0500

    hsdb

--- sw.js
+++ sw.js
@@ -17,3 +17,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11039685
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
