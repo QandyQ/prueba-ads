8511c2876a483f31fc3d2f0cbbae3f85522d52fe
QandyQ <edwin.quishpe@espoch.edu.ec>
2026-05-21 16:35:28 -0500

    ahora si

--- sw.js
+++ sw.js
@@ -11,3 +11,9 @@ self.options = {
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11030076
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
