ca0086b073e422a54af4d22b03d1807ed7dedb3a
QandyQ <edwin.quishpe@espoch.edu.ec>
2026-05-25 12:29:04 -0500

    isjajs

--- sw.js
+++ sw.js
@@ -24,3 +24,18 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11057271
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11057283
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
